5c27bd0eff9abdd302e530d4c94cf9a3a65fae0b
David Hewson <dev@daveid.co.uk>
2021-02-07 21:40:40 +0000

    strict fields on Header

--- src/Types.hs
+++ src/Types.hs
@@ -4,11 +4,11 @@ import Data.Text.Lazy (Text)
 
 data Header =
   Header
-  { hJob         :: Text
-  , hDate        :: Text
-  , hSampleUnit  :: Text
-  , hValueUnit   :: Text
-  , hSampleRange :: (Double, Double)
-  , hValueRange  :: (Double, Double)
-  , hCount       :: Int
+  { hJob         :: !Text
+  , hDate        :: !Text
+  , hSampleUnit  :: !Text
+  , hValueUnit   :: !Text
+  , hSampleRange :: !(Double, Double)
+  , hValueRange  :: !(Double, Double)
+  , hCount       :: !Int
   }
